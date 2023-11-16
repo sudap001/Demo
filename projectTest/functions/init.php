e6c7faba3d594f97a9fcf582abe180fe184da016
sudap001 <144737201+sudap001@users.noreply.github.com>
2023-11-15 20:26:40 -0500

    Delete projectTest directory

--- projectTest/functions/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-ob_start();
-session_start();
-include "db.php";
-include "ajax.php";
-include "functions.php";
--- projectTest/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'functions/init.php';
-session_destroy();
-
-if(isset($_COOKIE['email'])){
-    setcookie("email", "", time() - 3600);
-}
-
-redirect('index.php');
